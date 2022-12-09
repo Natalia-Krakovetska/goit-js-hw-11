b125eb04639c03274ab4e5fa419229e1a0c7c88f
Natalia <natalakrakoveckaya@gmail.com>
2022-12-09 06:59:27 +0100

    Summary

--- src/index.js
+++ src/index.js
@@ -1,82 +1,120 @@
-import Notiflix from 'notiflix';
+import './css/styles.css';
+import Notiflix, { Notify } from 'notiflix';
 import axios from 'axios';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 const form = document.querySelector('#search-form');
 const boxGallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 
-// form.addEventListener("submit", onSubmitForm);
-loadMoreBtn.addEventListener("click", onLoadMore)
-
-let instance = "";
+form.addEventListener('submit', onSubmitForm);
+loadMoreBtn.addEventListener('click', onLoadMore);
+loadMoreBtn.style.display = 'none';
 let page = 0;
+let total = 0;
+let searchQuery = '';
 
+function onSubmitForm(evt) {
+  evt.preventDefault();
+  loadMoreBtn.style.display = 'none';
+  page = 1;
 
+  clearGalleryList();
+  searchQuery = evt.currentTarget.elements.searchQuery.value.trim();
 
-// function onSubmitForm(evt){
-//     const evtTarget = evt.target.value;
+  if (!searchQuery) {
+    loadMoreBtn.style.display = 'none';
+    clearGalleryList();
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
 
-// }
-
-// fetch(`https://pixabay.com/api/?key=31910031-2af744f88dbcdc5739401f7e8&q=cats&image_type&orientation&safesearch`).then(resp => {  
-//     if(!resp.ok){
-//     throw new Error('Not ok')
-// }
-// return resp.json()})
-// .then(data => console.log(data))
-
-
-async function getUser(page = 1) {
-    try {
-      const response = await axios.get(`https://pixabay.com/api/?key=31910031-2af744f88dbcdc5739401f7e8&${page}&q=dog&image_type&orientation&safesearch`);
-      return response;
-    } catch (error) {
-      console.error(error);
-    }
+    clearGalleryList();
+    return;
   }
-  getUser().then(response =>{ 
-    console.log(response);
-    createmarkUp(response.data.hits)})
 
-function createmarkUp(arr){
-    const markUp = arr.map(item =>        
-`<div class="photo-card">
-<a class="gallery__item" href="${item.webformatURL}">
-  <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" width="300" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes ${item.likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views ${item.views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments ${item.comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads ${item.downloads}</b>
-    </p>
-  </div>
-  </a>
-</div>`
- ).join('');
- boxGallery.insertAdjacentHTML('beforeend', markUp) ;
+  getUser(searchQuery, page).then(response => {
+    if (!response.data.hits.length) {
+      loadMoreBtn.style.display = 'none';
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    createmarkUp(response.data.hits);
+    loadMoreBtn.style.display = 'block';
+  });
 }
 
+async function getUser() {
+  try {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=31910031-2af744f88dbcdc5739401f7e8&page=${page}&per_page=40&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`
+    );
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-// function onClickImage(evt){
-//     evt.preventDefault();
-//     instance.show();
-//     }
-// new SimpleLightbox('.gallery a', {captionsData: "alt",captionDelay: 250});
+function createmarkUp(arr) {
+  const markUp = arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${largeImageURL}">
+  <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" width="250" />
+  </a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${downloads}</b>
+    </p>
+  </div>
+ 
+</div>`
+    )
+    .join('');
+  boxGallery.insertAdjacentHTML('beforeend', markUp);
+}
 
+function onLoadMore() {
+  page += 1;
+  getUser(searchQuery, page)
+    .then(response => {
+      Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
+      total += response.data.hits.length;
 
+      if (total === Number(response.data.totalHits)) {
+        loadMoreBtn.style.display = 'none';
+        Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+      createmarkUp(response.data.hits);
+    })
+    .then(() => simpleligthbox.refresh());
+}
+const simpleligthbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
-
-function onLoadMore(){
-    page+=1;
-    loadMoreBtn.hidden = true;
-    getUser(page).then(response => {
-     createmarkUp(response.data.hits)})        
+function clearGalleryList() {
+  boxGallery.innerHTML = '';
 }
